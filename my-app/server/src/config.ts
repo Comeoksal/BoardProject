9d7a69864062d8ba7ff8765cfb3e6809b5f34fa1
Ksieon <keumsiun0503@gmail.com>
2025-02-04 19:08:24 +0900

    Delete my-app/server/src/config.ts

--- my-app/server/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-    PORT: 5000,
-    MongoURI: 'mongodb+srv://keumsiun0503:050329@ksieon.f3bwl.mongodb.net/?retryWrites=true&w=majority&appName=KSieon',
-    FRONT_SERVER: "http://localhost:3000",
-    BACK_SERVER: "https://dc1b-14-7-16-91.ngrok-free.app",
-}
